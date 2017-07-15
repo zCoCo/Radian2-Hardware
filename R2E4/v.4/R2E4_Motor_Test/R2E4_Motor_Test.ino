fdcbc65e4ff28dcdd4ce02b1efa2e6c5d130ca34
zCoCo <connor.caxd@gmail.com>
2017-07-15 09:21:21 -0400

    Fully Minified AccelStepper

--- R2E4/v.4/R2E4_Motor_Test/R2E4_Motor_Test.ino
+++ R2E4/v.4/R2E4_Motor_Test/R2E4_Motor_Test.ino
@@ -8,14 +8,23 @@
 // $Id: ConstantSpeed.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 
 #include <AccelStepper_Mini.h>
+#include <I2C.h>
+#include "t48_Pinout.h"
 
+//Potentiometer Base Address
+#define POT_ADDR 0b0101000
+//Potentiometer Address Setting Pins
+#define POT_ADDR0 PD6
+#define POT_ADDR1 PC7
+#define POT_ADDR2 PC3
 
-//PDO
-#define STP 0
-//PD1
-#define DIR 1
-//PD2
-#define _EN_ 2
+#define STP PD0
+#define DIR PD1
+#define _EN_ PD2
+#define MS1 PD5
+#define MS2 PB7
+#define MS3 PB6
+
 AccelStepper_Mini stepper(STP, DIR);
 
 //PB0
@@ -23,21 +32,39 @@ AccelStepper_Mini stepper(STP, DIR);
 //PB1
 #define LED_K 9
 
-void setup()
-{
+void setup() {
+  I2c.begin(); // join i2c bus
+   //Set Potentiometer Address:
+   pinMode(POT_ADDR0, OUTPUT);
+    digitalWrite(POT_ADDR0, POT_ADDR & 1);
+   pinMode(POT_ADDR1, OUTPUT);
+    digitalWrite(POT_ADDR1, (POT_ADDR >> 1) & 1);
+   pinMode(POT_ADDR2, OUTPUT);
+    digitalWrite(POT_ADDR2, (POT_ADDR >> 2) & 1);
+   // Set Potentiometer Current:
+   I2c.write(POT_ADDR, 0x00, 127); //Pot Value Byte (2 Amps)
+
+  // Setup Stepper Driver:
+  pinMode(MS1, OUTPUT);
+   digitalWrite(MS1, LOW);
+  pinMode(MS2, OUTPUT);
+   digitalWrite(MS2, LOW);
+  pinMode(MS3, OUTPUT);
+   digitalWrite(MS3, LOW);
   pinMode(_EN_, OUTPUT);
    digitalWrite(_EN_, LOW);
 
-  pinMode(LED_A, OUTPUT);
-   digitalWrite(LED_A, HIGH);
-  pinMode(LED_K, OUTPUT);
-   digitalWrite(LED_K, LOW);
-
+   // Setup Stepping Characteristics:
    stepper.setMaxSpeed(1000);
-   stepper.setSpeed(50);
+   stepper.setSpeed(800);
+
+   //Indicator LED:
+   pinMode(LED_A, OUTPUT);
+    digitalWrite(LED_A, HIGH);
+   pinMode(LED_K, OUTPUT);
+    digitalWrite(LED_K, LOW);
 }
 
-void loop()
-{
-   stepper.runSpeed();
+void loop() {
+  stepper.runSpeed();
 }
---
+++ R2E4/v.4/R2E4_Motor_Test/t48_Pinout.h
@@ -0,0 +1,36 @@
+#ifndef T48_PINOUT_H
+#define T48_PINOUT_H
+
+#define PA0 23 /* A6 */
+#define PA1 24 /* A7 */
+#define PA2 25
+#define PA3 26
+
+#define PB0 8
+#define PB1 9 /* OC1A */
+#define PB2 10 /* OC1B/_SS_ */
+#define PB3 11 /* MOSI */
+#define PB4 12 /* MISO */
+#define PB5 13 /* SCK */
+#define PB6 14
+#define PB7 15
+
+#define PC0 17 /* A0 */
+#define PC1 18 /* A1 */
+#define PC2 19 /* A2 */
+#define PC3 20 /* A3 */
+#define PC4 21 /* A4/SDA */
+#define PC5 22 /* A5/SCL */
+#define PC6 26 /* RST */
+#define PC7 16
+
+#define PDO 0
+#define PD1 1
+#define PD2 2 /* INT0 */
+#define PD3 3 /* INT1 */
+#define PD4 4
+#define PD5 5
+#define PD6 6 /* AIN0 */
+#define PD7 7 /* AIN1 */
+
+#endif //T48_PINOUT_H
